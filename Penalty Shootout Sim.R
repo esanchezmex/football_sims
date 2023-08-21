8e24df945d3a79127d4ecb24e00f8146425d99d4
Esteban Sanchez <109927620+esanchezmex@users.noreply.github.com>
2023-08-21 19:34:26 +0200

    Created initial files including gitingore file, and added first updates to penalty simulation

---
+++ Penalty Shootout Sim.R
@@ -0,0 +1,84 @@
+# Function: Simulate one penalty
+one_pen <- function(){
+  prob <- runif(n = 1)
+  return(prob)
+}
+
+# Function: Turn probability into result
+goal_or_no <- function(prob){
+  if (prob <= 0.725){
+    result = 1
+  } else {
+    result = 0
+  }
+  return(result)
+}
+
+
+# Function: Check if a team won
+check_winner <- function(team_a_count, team_b_count, total_pens){
+  if (team_a_count > team_b_count){
+    winner = 1
+    return(cat("Team A wins \n") +  cat(c("Team A" = team_a_count, "Team B" = team_b_count, "Total Pens" = total_pens, "\n"))+ winner)
+  } else if (team_b_count > team_a_count){
+    winner = 1
+    return(cat("Team B wins \n") + cat(c("Team A" = team_a_count, "Team B" = team_b_count, "Total Pens" = total_pens, '\n')) + winner)
+  } else {
+    winner = 0
+    return(winner)
+  }
+}
+
+
+# Function: In case of tie, sudden death
+sudden_death <- function(team_a_count, team_b_count, total_pens){
+  while (team_a_count == team_b_count | team_b_count == team_a_count){
+    team_a_shot <- goal_or_no(one_pen())
+    team_a_count = team_a_count + team_a_shot
+    
+    team_b_shot <- goal_or_no(one_pen())
+    team_b_count = team_b_count + team_b_shot
+    
+    total_pens = total_pens + 2
+  }
+  check_winner(team_a_count, team_b_count, total_pens)
+}
+
+# Function: Translate results into penalty shootout rules
+shootout <- function(){
+  team_a_count = 0
+  team_b_count = 0
+  total_pens = 0
+  for (pen_number in 1:5){
+    team_a_shot <- goal_or_no(one_pen())
+    team_a_count = team_a_count + team_a_shot
+    
+    team_b_shot <- goal_or_no(one_pen())
+    team_b_count = team_b_count + team_b_shot
+    
+    total_pens = total_pens + 2
+  }
+  winner = check_winner(team_a_count, team_b_count, total_pens)[2]
+  if ( winner == 1){
+    cat("Winner")
+  } else {
+    sudden_death(team_a_count, team_b_count, total_pens)
+  }
+}
+
+shootout()
+check_winner(2, 3, 4)
+
+
+
+###############
+# DEV
+test_func <- function(){
+  team_a_goals = 0
+  team_b_goals = 0
+  rounds_played = 0
+  
+  while(rounds_played < 5 | team_a_goals == team_b_goals){
+  
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # football_sims
+
+This repo will follow my attmempts in creating football simulations based on the book Monte Carlo or Bust by Joseph Buchdahl.
